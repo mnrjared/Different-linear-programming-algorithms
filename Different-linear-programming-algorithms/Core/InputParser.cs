559c32b2237fb6dd5f9dea0db37d62918027bdfb
mnrjared <90599342+mnrjared@users.noreply.github.com>
2026-08-08 13:32:53 +0200

    made the core files will add code later

---
+++ Different-linear-programming-algorithms/Core/InputParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Different_linear_programming_algorithms.Core
+{
+    internal class InputParser
+    {
+    }
+}
